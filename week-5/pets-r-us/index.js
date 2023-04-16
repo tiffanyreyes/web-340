8334a0ebb4583faf7e7824577a9d93db54b69ab0
Tiffany Reyes <tiffanyreyes.tr@outlook.com>
2023-04-15 23:42:35 -0400

    adding assignment 5.2

---
+++ week-5/pets-r-us/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+
+const app = express(); 
+
+
+app.set('views', path.join(__dirname, 'views')); 
+app.set('view engine', 'ejs'); 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Pets-R-Us',
+    });
+});
+
+app.get('/grooming', (req, res) => {
+    res.render('grooming', {
+        title: 'Pets-R-Us',
+    })
+});
+
+app.get('/boarding', (req, res) => {
+    res.render('boarding', {
+        title: 'Pets-R-Us',
+    })
+});
+
+app.get('/training', (req, res) => {
+    res.render('training', {
+        title: 'Pets-R-Us',
+    })
+});
+
+app.listen(PORT, () => {});
